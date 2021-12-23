5a33cced5e97d9ee2c51780513082e36ee65c5de
Florianisme <florian.m9782@gmail.com>
2021-12-23 18:39:26 +0100

    Add Validation of inputs

--- app/src/main/java/de/florianisme/wakeonlan/home/addmachine/AddMachineActivity.java
+++ app/src/main/java/de/florianisme/wakeonlan/home/addmachine/AddMachineActivity.java
@@ -3,12 +3,18 @@ package de.florianisme.wakeonlan.home.addmachine;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 import de.florianisme.wakeonlan.R;
 import de.florianisme.wakeonlan.databinding.ActivityAddMachineBinding;
+import de.florianisme.wakeonlan.home.addmachine.validator.BroadcastValidator;
+import de.florianisme.wakeonlan.home.addmachine.validator.MacValidator;
+import de.florianisme.wakeonlan.home.addmachine.validator.NameValidator;
 import de.florianisme.wakeonlan.persistence.AppDatabase;
 import de.florianisme.wakeonlan.persistence.DatabaseInstanceManager;
 import de.florianisme.wakeonlan.persistence.Machine;
@@ -18,6 +24,9 @@ public class AddMachineActivity extends AppCompatActivity {
     private ActivityAddMachineBinding binding;
     private AppDatabase databaseInstance;
 
+    private TextInputEditText machineMacInput;
+    private TextInputEditText machineNameInput;
+    private TextInputEditText machineBroadcastInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +35,20 @@ public class AddMachineActivity extends AppCompatActivity {
         binding = ActivityAddMachineBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        machineMacInput = binding.machine.machineMac;
+        machineNameInput = binding.machine.machineName;
+        machineBroadcastInput = binding.machine.machineBroadcast;
+
         setSupportActionBar(binding.toolbar);
 
         databaseInstance = DatabaseInstanceManager.getDatabaseInstance();
+        addValidators();
+    }
+
+    private void addValidators() {
+        machineMacInput.addTextChangedListener(new MacValidator(machineMacInput));
+        machineNameInput.addTextChangedListener(new NameValidator(machineNameInput));
+        machineBroadcastInput.addTextChangedListener(new BroadcastValidator(machineBroadcastInput));
     }
 
     @Override
@@ -40,20 +60,30 @@ public class AddMachineActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == R.id.add_machine_menu_save) {
-            persistMachine();
-            finish();
-            return true;
+            if (assertInputsNotEmptyAndValid()) {
+                persistMachine();
+                finish();
+                return true;
+            } else {
+                Toast.makeText(this, R.string.add_machine_error_save_clicked, Toast.LENGTH_LONG).show();
+            }
         }
 
         return super.onOptionsItemSelected(item);
 
     }
 
+    private boolean assertInputsNotEmptyAndValid() {
+        return machineMacInput.getError() == null && machineMacInput.getText().length() != 0 &&
+                machineNameInput.getError() == null && machineNameInput.getText().length() != 0 &&
+                machineBroadcastInput.getError() == null && machineBroadcastInput.getText().length() != 0;
+    }
+
     private void persistMachine() {
         Machine machine = new Machine();
-        machine.name = binding.machine.machineName.getText().toString();
-        machine.macAddress = binding.machine.machineMac.getText().toString();
-        machine.broadcast_address = binding.machine.machineBroadcast.getText().toString();
+        machine.name = machineNameInput.getText().toString();
+        machine.macAddress = machineMacInput.getText().toString();
+        machine.broadcast_address = machineBroadcastInput.getText().toString();
         machine.port = getPort();
 
         databaseInstance.userDao().insertAll(machine);
---
+++ app/src/main/java/de/florianisme/wakeonlan/home/addmachine/validator/BroadcastValidator.java
@@ -0,0 +1,33 @@
+package de.florianisme.wakeonlan.home.addmachine.validator;
+
+import android.widget.EditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import de.florianisme.wakeonlan.R;
+
+public class BroadcastValidator extends Validator {
+
+    private final Pattern IP_PATTERN = Pattern.compile("^(?:[0-9]{1,3}\\.){3}[0-9]{1,3}$");
+
+    public BroadcastValidator(EditText editTextView) {
+        super(editTextView);
+    }
+
+    @Override
+    public ValidationResult validate(String text) {
+        Matcher matcher = IP_PATTERN.matcher(text);
+        if (matcher.matches()) {
+            return ValidationResult.VALID;
+        } else {
+            return ValidationResult.INVALID;
+        }
+    }
+
+    @Override
+    int getErrorMessageStringId() {
+        return R.string.add_machine_error_broadcast_invalid;
+    }
+
+}
