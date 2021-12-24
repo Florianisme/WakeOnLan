1ec360d0e35ee4152b23281abd2beee6393781ba
Florianisme <florian.m9782@gmail.com>
2021-12-24 12:25:01 +0100

    Add autofill for broadcast address

--- app/src/main/java/de/florianisme/wakeonlan/home/addmachine/ModifyMachineActivity.java
+++ app/src/main/java/de/florianisme/wakeonlan/home/addmachine/ModifyMachineActivity.java
@@ -1,10 +1,16 @@
 package de.florianisme.wakeonlan.home.addmachine;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.Optional;
 
 import de.florianisme.wakeonlan.databinding.ActivityModifyMachineBinding;
 import de.florianisme.wakeonlan.home.addmachine.validator.BroadcastValidator;
@@ -12,6 +18,7 @@ import de.florianisme.wakeonlan.home.addmachine.validator.MacValidator;
 import de.florianisme.wakeonlan.home.addmachine.validator.NameValidator;
 import de.florianisme.wakeonlan.persistence.AppDatabase;
 import de.florianisme.wakeonlan.persistence.DatabaseInstanceManager;
+import de.florianisme.wakeonlan.util.BroadcastHelper;
 
 public abstract class ModifyMachineActivity extends AppCompatActivity {
 
@@ -37,6 +44,21 @@ public abstract class ModifyMachineActivity extends AppCompatActivity {
 
         databaseInstance = DatabaseInstanceManager.getDatabaseInstance();
         addValidators();
+        addAutofillClickHandler();
+    }
+
+    private void addAutofillClickHandler() {
+        binding.machine.broadcastAutofill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Optional<InetAddress> broadcastAddress = BroadcastHelper.getBroadcastAddress(ModifyMachineActivity.this);
+                    broadcastAddress.ifPresent(inetAddress -> binding.machine.machineBroadcast.setText(inetAddress.getHostAddress()));
+                } catch (IOException e) {
+                    Log.e(this.getClass().getName(), "Can not retrieve Broadcast Address", e);
+                }
+            }
+        });
     }
 
     private void addValidators() {
---
+++ app/src/main/java/de/florianisme/wakeonlan/util/BroadcastHelper.java
@@ -0,0 +1,28 @@
+package de.florianisme.wakeonlan.util;
+
+import android.content.Context;
+import android.net.DhcpInfo;
+import android.net.wifi.WifiManager;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.Optional;
+
+public class BroadcastHelper {
+
+    public static Optional<InetAddress> getBroadcastAddress(Context context) throws IOException {
+        WifiManager wifi = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        DhcpInfo dhcp = wifi.getDhcpInfo();
+
+        if (dhcp == null) {
+            return Optional.empty();
+        }
+
+        int broadcast = (dhcp.ipAddress & dhcp.netmask) | ~dhcp.netmask;
+        byte[] quads = new byte[4];
+        for (int k = 0; k < 4; k++)
+            quads[k] = (byte) (broadcast >> (k * 8));
+        return Optional.of(InetAddress.getByAddress(quads));
+    }
+
+}
