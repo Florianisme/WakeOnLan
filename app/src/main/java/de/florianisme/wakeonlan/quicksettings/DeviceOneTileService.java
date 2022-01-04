79161adee4f7eb9ab39633ef6a5af4202cf6aca9
Florianisme <florian.m9782@gmail.com>
2022-01-04 11:56:44 +0100

    Cleanup

--- app/src/main/java/de/florianisme/wakeonlan/quicksettings/DeviceOneTileService.java
+++ app/src/main/java/de/florianisme/wakeonlan/quicksettings/DeviceOneTileService.java
@@ -1,6 +1,7 @@
 package de.florianisme.wakeonlan.quicksettings;
 
 public class DeviceOneTileService extends DeviceTileService {
+
     @Override
     int machineAtIndex() {
         return 0;
--- app/src/main/java/de/florianisme/wakeonlan/quicksettings/DeviceTwoTileService.java
+++ app/src/main/java/de/florianisme/wakeonlan/quicksettings/DeviceTwoTileService.java
@@ -1,6 +1,7 @@
 package de.florianisme.wakeonlan.quicksettings;
 
 public class DeviceTwoTileService extends DeviceTileService {
+
     @Override
     int machineAtIndex() {
         return 1;
