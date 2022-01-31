b00b623df4be700be9d65249f0c2b2ba61e0bf0e
Florianisme <florian.m9782@gmail.com>
2022-01-31 20:53:16 +0100

    Cleanup binding declaration

--- app/src/main/java/de/florianisme/wakeonlan/home/MainActivity.java
+++ app/src/main/java/de/florianisme/wakeonlan/home/MainActivity.java
@@ -14,13 +14,12 @@ import de.florianisme.wakeonlan.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbar);
