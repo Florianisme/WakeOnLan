db2dc6ee64341e7b699c59a8504d66713f120941
Florianisme <florian.m9782@gmail.com>
2023-10-02 17:56:29 +0200

    Add splash screen to Wear OS app to comply with guidelines

---
+++ wear/src/main/java/de/florianisme/wakeonlan/SplashActivity.java
@@ -0,0 +1,20 @@
+package de.florianisme.wakeonlan;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, DeviceListActivity.class);
+        startActivity(intent);
+
+        finish();
+    }
+}
