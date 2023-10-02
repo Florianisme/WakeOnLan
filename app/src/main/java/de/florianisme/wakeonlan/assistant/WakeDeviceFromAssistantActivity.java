bdc1dc73c8f8d16f17bc1cce20fcec8a13e6cd09
Florianisme <florian.m9782@gmail.com>
2023-10-02 13:15:05 +0200

    Initial Google Assistant integration setup

---
+++ app/src/main/java/de/florianisme/wakeonlan/assistant/WakeDeviceFromAssistantActivity.java
@@ -0,0 +1,14 @@
+package de.florianisme.wakeonlan.assistant;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WakeDeviceFromAssistantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
