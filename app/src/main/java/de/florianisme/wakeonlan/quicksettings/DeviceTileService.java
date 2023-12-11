7cbd3266541768b2b89e3ee5cafd651acef58cd2
Florianisme <florian.m9782@gmail.com>
2023-12-11 19:35:29 +0100

    #29 Shutdown/Wake device based on status

--- app/src/main/java/de/florianisme/wakeonlan/quicksettings/DeviceTileService.java
+++ app/src/main/java/de/florianisme/wakeonlan/quicksettings/DeviceTileService.java
@@ -3,7 +3,6 @@ package de.florianisme.wakeonlan.quicksettings;
 import android.os.Build;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
-import android.util.Log;
 import android.widget.Toast;
 
 import java.util.List;
@@ -13,6 +12,7 @@ import de.florianisme.wakeonlan.R;
 import de.florianisme.wakeonlan.persistence.models.Device;
 import de.florianisme.wakeonlan.persistence.models.DeviceStatus;
 import de.florianisme.wakeonlan.persistence.repository.DeviceRepository;
+import de.florianisme.wakeonlan.shutdown.ShutdownExecutor;
 import de.florianisme.wakeonlan.ui.list.status.DeviceStatusListener;
 import de.florianisme.wakeonlan.ui.list.status.pool.PingStatusTesterPool;
 import de.florianisme.wakeonlan.ui.list.status.pool.StatusTestType;
@@ -25,6 +25,8 @@ public abstract class DeviceTileService extends TileService implements DeviceSta
 
     private DeviceRepository deviceRepository;
     private Device device;
+
+    private DeviceStatus lastDeviceStatus = DeviceStatus.UNKNOWN;
 
 
     @Override
@@ -68,15 +70,20 @@ public abstract class DeviceTileService extends TileService implements DeviceSta
         Tile tile = getQsTile();
         tile.setState(deviceStatus == DeviceStatus.ONLINE ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
         tile.updateTile();
+
+        lastDeviceStatus = deviceStatus;
     }
 
     @Override
     public void onClick() {
-        try {
+        if (lastDeviceStatus == DeviceStatus.ONLINE) {
+            if (device.remoteShutdownEnabled) {
+                ShutdownExecutor.shutdownDevice(device);
+                Toast.makeText(this, getString(R.string.remote_shutdown_send_command, device.name), Toast.LENGTH_LONG).show();
+            }
+        } else {
             WolSender.sendWolPacket(device);
             Toast.makeText(this, getString(R.string.wol_toast_sending_packet, device.name), Toast.LENGTH_LONG).show();
-        } catch (Exception e) {
-            Log.e(this.getClass().getName(), "Error while sending WOL Packet", e);
         }
     }
 
